d82745477cd40e4e2b98cdbc7d42ef3824265e42
yalin <yalincobanoglu@yandex.com>
2018-09-25 13:46:43 +0300

    admin routes

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', 'Admin\DashboardController@index')->name('dashboard');
+
+Route::middleware('auth')->namespace('Admin')->group(function(){
+    //dashboard
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+    //source
+    Route::get('/source', 'SourceController@index')->name('source.index');
+    Route::get('/source/create', 'SourceController@create')->name('source.create');
+    Route::post('/source/store', 'SourceController@store')->name('source.store');
+    Route::delete('source', 'SourceController@destroy')->name('source.destroy');
+});
