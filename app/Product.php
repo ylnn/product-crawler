ff1ef6b486ade1e5793b32ae7ae1830bbda1660c
yalin <yalincobanoglu@yandex.com>
2018-09-25 22:27:00 +0300

    variant relation

--- app/Product.php
+++ app/Product.php
@@ -19,4 +19,9 @@ class Product extends Model
     {
         return $this->hasMany(Image::class);
     }
+
+    public function variants()
+    {
+        return $this->hasMany(Variant::class); 
+    }
 }
