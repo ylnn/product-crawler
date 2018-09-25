b7108271adf9cf3afc8cf284499ce65fa62d3e9b
yalin <yalincobanoglu@yandex.com>
2018-09-25 15:42:33 +0300

    product model and migration

---
+++ app/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'title',
+        'handle',
+        'body_html',
+        'vendor',
+        'product_type',
+    ];
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+}
---
+++ database/migrations/2018_09_25_105233_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('product_id')->unsigned()->unique();
+            $table->string('title');
+            $table->string('handle')->nullable();
+            $table->text('body_html')->nullable();
+            $table->string('vendor')->nullable();
+            $table->string('product_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
