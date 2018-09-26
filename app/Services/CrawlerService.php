a5254dafe3001ff2fd8927615ee81f7f56ea1441
yalin <yalincobanoglu@yandex.com>
2018-09-26 13:46:07 +0300

    crawler service

--- app/Services/CrawlerService.php
+++ app/Services/CrawlerService.php
@@ -2,8 +2,36 @@
 
 namespace App\Services;
 
+use App\Source;
+use App\Services\ProductService;
+use Illuminate\Support\Facades\Log;
+
 class CrawlerService
 {
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+    public function start()
+    {
+        $urls = Source::all();
+
+        if ($urls->count() === 0) {
+            $message = "No url found.";
+            Log::error($message);
+            return $message;
+        }
+
+        $urls->each(function ($item) {
+            $jsonData = $this->crawl($item->url);
+            $this->productService->saveProducts($jsonData->products);
+        });
+
+        return true;
+    }
+
     public function crawl($url) : object
     {
         $client = new \GuzzleHttp\Client();
